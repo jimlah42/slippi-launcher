4c4977ff945b9aaa5ae8879558c4661d6a14f02d
Vince Au <vinceau09@gmail.com>
2022-11-05 11:01:12 +1100

    update stories

--- src/renderer/components/player_badge/PlayerBadge.stories.tsx
+++ src/renderer/components/player_badge/PlayerBadge.stories.tsx
@@ -1,5 +1,7 @@
 import { PlayerBadge } from "./PlayerBadge";
 
+type PlayerBadgeProps = React.ComponentProps<typeof PlayerBadge>;
+
 export default {
   title: "components/PlayerBadge",
   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
@@ -7,13 +9,44 @@ export default {
 };
 
 export const NullValues = () => {
-  return <PlayerBadge characterId={null} characterColor={null} port={1} text="Unknown" />;
+  return generateBadge({ characterId: null, characterColor: null });
 };
 
 export const TagVariant = () => {
-  return <PlayerBadge variant="tag" characterId={20} characterColor={0} port={1} text="Player 1" />;
+  return generateBadge({});
 };
 
 export const ConnectCodeVariant = () => {
-  return <PlayerBadge variant="code" characterId={20} characterColor={0} port={1} text="ABC#123" />;
+  return generateBadge({ variant: "code", text: "ABC#123" });
 };
+
+export const CharacterBadges = () => {
+  const badges: React.ReactNode[] = [];
+  for (let i = 0; i <= 25; i++) {
+    badges.push(<div key={i}>{generateBadgeRow({ characterId: i })}</div>);
+  }
+  return <div>{badges}</div>;
+};
+
+const defaultProps: PlayerBadgeProps = {
+  variant: "tag",
+  characterId: 20,
+  characterColor: 0,
+  port: 1,
+  text: "Player 1",
+};
+
+function generateBadge(options: Partial<PlayerBadgeProps>) {
+  return <PlayerBadge {...defaultProps} {...options} />;
+}
+
+function generateBadgeRow(options: Partial<PlayerBadgeProps>) {
+  const badges = [1, 2, 3, 4].map((port) => {
+    return (
+      <div style={{ display: "inline-block", padding: "0 5px" }} key={port}>
+        {generateBadge({ ...options, port, text: `Player ${port}` })}
+      </div>
+    );
+  });
+  return <div style={{ padding: "5px 0" }}>{badges}</div>;
+}
